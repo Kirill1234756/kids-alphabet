b28f9fcc144821eb07d92eab77bb0b189e269b13
Kirill1234756 <154319021+Kirill1234756@users.noreply.github.com>
2024-11-05 17:52:36 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <main>
+    <componentHeader />
+  </main>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import componentHeader from './components/componentHeader.vue';
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/componentHeader.vue
@@ -0,0 +1,67 @@
+<template>
+
+    <header class="">
+        <nav class="w-full px-20 py-4 flex items-center justify-between">
+            <div
+        class="!visible hidden grow basis-[100%] items-center text-center lg:!flex lg:basis-auto lg:text-left"
+        id="navbarSupportedContentY"
+        data-twe-collapse-item>
+        <ul
+          class="me-auto flex flex-col lg:flex-row"
+          data-twe-navbar-nav-ref>
+          <li class="mb-4 lg:mb-0 lg:pe-2" data-twe-nav-item-ref>
+            <a
+              class="block text-black/60 transition duration-200 hover:text-black/80 hover:ease-in-out focus:text-black/80 active:text-black/80 motion-reduce:transition-none dark:text-white/60 dark:hover:text-white/80 dark:focus:text-white/80 dark:active:text-white/80 lg:px-2"
+              href="#!"
+              data-twe-nav-link-ref
+              data-twe-ripple-init
+              data-twe-ripple-color="light"
+              >Home</a
+            >
+          </li>
+          <li class="mb-4 lg:mb-0 lg:pe-2" data-twe-nav-item-ref>
+            <a
+              class="block text-black/60 transition duration-200 hover:text-black/80 hover:ease-in-out focus:text-black/80 active:text-black/80 motion-reduce:transition-none dark:text-white/60 dark:hover:text-white/80 dark:focus:text-white/80 dark:active:text-white/80 lg:px-2"
+              href="#!"
+              data-twe-nav-link-ref
+              data-twe-ripple-init
+              data-twe-ripple-color="light"
+              >Features</a
+            >
+          </li>
+          <li class="mb-4 lg:mb-0 lg:pe-2" data-twe-nav-item-ref>
+            <a
+              class="block text-black/60 transition duration-200 hover:text-black/80 hover:ease-in-out focus:text-black/80 active:text-black/80 motion-reduce:transition-none dark:text-white/60 dark:hover:text-white/80 dark:focus:text-white/80 dark:active:text-white/80 lg:px-2"
+              href="#!"
+              data-twe-nav-link-ref
+              data-twe-ripple-init
+              data-twe-ripple-color="light"
+              >Pricing</a
+            >
+          </li>
+          <li class="mb-2 lg:mb-0 lg:pe-2" data-twe-nav-item-ref>
+            <a
+              class="block text-black/60 transition duration-200 hover:text-black/80 hover:ease-in-out focus:text-black/80 active:text-black/80 motion-reduce:transition-none dark:text-white/60 dark:hover:text-white/80 dark:focus:text-white/80 dark:active:text-white/80 lg:px-2"
+              href="#!"
+              data-twe-nav-link-ref
+              data-twe-ripple-init
+              data-twe-ripple-color="light"
+              >About</a
+            >
+          </li>
+        </ul>
+      </div>
+
+        </nav>
+
+    </header>
+  
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
